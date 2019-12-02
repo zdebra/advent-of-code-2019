8ef042183b1ec4dbcbe3e1cd9d49699e8faad684
zdebra <zd.brabec@gmail.com>
2019-12-02 20:34:53 +0100

    task 01 done

---
+++ task01/main.go
@@ -0,0 +1,59 @@
+// https://adventofcode.com/2019/day/1
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+type Module struct {
+	Mass float64
+}
+
+func (m Module) FuelRequiredToLaunch() float64 {
+	return math.Trunc(	m.Mass / 3 ) - 2
+}
+
+type Fleet []Module
+
+func (f Fleet) FuelRequiredToLaunch() float64 {
+	var total float64
+	for _, module := range f {
+		total += module.FuelRequiredToLaunch()
+	}
+	return total
+}
+
+func calculateFleetFuel(r io.Reader) (float64, error) {
+	sc := bufio.NewScanner(r)
+	f := Fleet{}
+	for sc.Scan() {
+		mass, err := strconv.ParseFloat(sc.Text(), 64)
+		if err != nil {
+			return 0, fmt.Errorf("failed to parse mass: %w", err)
+		}
+		f = append(f, Module{Mass:mass})
+	}
+	return f.FuelRequiredToLaunch(), nil
+}
+
+func main() {
+	f, err := os.Open("task01/input")
+	panicOnErr(err)
+
+	fleetFuel, err := calculateFleetFuel(f)
+	panicOnErr(err)
+
+	fmt.Printf("total mass required for fleet is %f\n", fleetFuel)
+
+}
+
+func panicOnErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ task01/main_test.go
@@ -0,0 +1,53 @@
+package main
+
+import "testing"
+
+func TestModule_FuelRequiredToLaunch(t *testing.T) {
+	type fields struct {
+		Mass float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   float64
+	}{
+		{
+			name:   "example 1",
+			fields: fields{
+				Mass: 12,
+			},
+			want:   2,
+		},
+		{
+			name:   "example 2",
+			fields: fields{
+				Mass: 14,
+			},
+			want:   2,
+		},
+		{
+			name:   "example 3",
+			fields: fields{
+				Mass: 1969,
+			},
+			want:   654,
+		},
+		{
+			name:   "example 4",
+			fields: fields{
+				Mass: 100756,
+			},
+			want:   33583,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			m := Module{
+				Mass: tt.fields.Mass,
+			}
+			if got := m.FuelRequiredToLaunch(); got != tt.want {
+				t.Errorf("FuelRequiredToLaunch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
