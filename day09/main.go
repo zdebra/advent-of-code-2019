d7d37c53d4ac56ea9bb37c3738edac3e2ccc9d03
zdebra <zd.brabec@gmail.com>
2019-12-21 21:18:28 +0100

    day09-fail

--- day09/main.go
+++ day09/main.go
@@ -31,17 +31,21 @@ type program struct {
 
 func (p *program) Read(posDelta int, mode int) int {
 	pos := p.ip + posDelta
+
+	var retValue int
 	switch mode {
 	case MR:
 		pos = p.relativeBase + posDelta
 		fallthrough
 	case MP:
-		return p.memory[p.memory[pos]]
+		retValue = p.memory[p.memory[pos]]
 	case MI:
-		return p.memory[pos]
+		retValue = p.memory[pos]
 	default:
 		panic(fmt.Sprintf("unsupported mode %d", mode))
 	}
+	fmt.Println("read:", "pos", pos, "mode", mode, "retValue", retValue, "posDelta", posDelta)
+	return retValue
 }
 
 func (p *program) Write(pos, value int) {
@@ -102,6 +106,7 @@ func (p *program) run() bool {
 		return true
 	}
 	nextIp := p.ip + opcode.size()
+	fmt.Println("opcode", opcode)
 	switch opcode {
 	case add:
 		p.Write(p.Read(3, MI), p.Read(1, modes[0])+p.Read(2, modes[1]))
@@ -110,9 +115,9 @@ func (p *program) run() bool {
 	case input:
 		p.Write(p.Read(1, MI), p.input.read())
 	case output:
-		p.output = p.Read(1, MP)
+		p.output = p.Read(1, modes[0])
 		p.ip = nextIp
-		return false
+		fmt.Println("outputing", p.output)
 	case jumpIfTrue:
 		if p.Read(p.ip+1, modes[0]) != 0 {
 			nextIp = p.Read(2, modes[1])
@@ -138,7 +143,8 @@ func (p *program) run() bool {
 		}
 		p.Write(p.Read(3, MI), result)
 	case adjustRelativeBase:
-		p.relativeBase = p.relativeBase + p.Read(0, MI)
+		p.relativeBase = p.relativeBase + p.Read(1, MI)
+		fmt.Println("relative base", p.relativeBase)
 	default:
 		panic(fmt.Sprintf("unsupported opcode %d", opcode))
 	}
@@ -146,92 +152,22 @@ func (p *program) run() bool {
 	return p.run()
 }
 
-func programChain(initInput int, phaseSetting []int) int {
-	previousPhaseOutput := initInput
-	for _, phase := range phaseSetting {
-		p := program{
-			memory: startingMemory(),
-			input: &programInput{
-				m: [2]int{phase, previousPhaseOutput},
-			},
-		}
-		p.run()
-		previousPhaseOutput = p.output
-	}
-	return previousPhaseOutput
-}
-
-func programLoop(initInput int, phaseSetting []int) int {
-	programs := make([]*program, 5)
-	// init programs
-	for i, phase := range phaseSetting {
-		programs[i] = &program{
-			memory: startingMemory(),
-			input: &programInput{
-				m: [2]int{phase},
-			},
-		}
-	}
-
-	// loop until halt
-	previousPhaseOutput := initInput
-	halted := false
-	for !halted {
-		for _, prg := range programs {
-			prg.input.m[1] = previousPhaseOutput
-			halted = prg.run()
-			previousPhaseOutput = prg.output
-		}
-	}
-	return programs[4].output
-}
-
 func main() {
-	maxInput := splitInt(56789)
-	max := programLoop(0, maxInput)
-	for i := 0; i <= 99999; i++ {
-		curInp := splitInt(i)
-		if !matchCriteria(curInp) {
-			continue
-		}
-		curOutput := programLoop(0, curInp)
-		if curOutput > max {
-			max = curOutput
-			maxInput = curInp
-		}
+
+	p := program{
+		memory:       startingMemory(1000),
+		ip:           0,
+		relativeBase: 0,
 	}
-	fmt.Println(max, maxInput)
-	if max != 1336480 {
-		panic("hooooowdy")
-	}
+
+	halt := p.run()
+	fmt.Println(halt, p.output)
+
 }
 
-func matchCriteria(inp []int) bool {
-	for i := 0; i < len(inp); i++ {
-		if inp[i] < 5 {
-			return false
-		}
-		for j := i + 1; j < len(inp); j++ {
-			if inp[j] < 5 {
-				return false
-			}
-			if inp[i] == inp[j] {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func splitInt(inp int) []int {
-	res := make([]int, 5)
-	for i := 4; i >= 0; i-- {
-		res[i] = inp % 10
-		inp /= 10
-	}
-	return res
-}
-
-func startingMemory() []int {
-	return []int{3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 38, 55, 64, 81, 106, 187, 268, 349, 430, 99999, 3, 9, 101, 2, 9, 9, 1002, 9, 2, 9, 101, 5, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 101, 3, 9, 9, 1002, 9, 4, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 5, 9, 1001, 9, 4, 9, 102, 4, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 1001, 9, 5, 9, 102, 3, 9, 9, 1001, 9, 4, 9, 102, 5, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99}
+func startingMemory(programSpace int) []int {
+	m := []int{109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99}
+	zeros := make([]int, programSpace)
+	m = append(m, zeros...)
+	return m
 }
